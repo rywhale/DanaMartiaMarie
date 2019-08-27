da6aa32cb0bd24906627fb41473f31cb4e5a0b6c
rywhale <rdgwhaley@gmail.com>
2019-08-27 10:53:54 -0400

    Initial commit

---
+++ app.R
@@ -0,0 +1,195 @@
+# Imports
+library(shiny)
+library(rtdd)
+library(dplyr)
+library(lubridate)
+library(leaflet)
+library(leaflet.esri)
+library(sf)
+library(ggplot2)
+library(tidyr)
+library(stringr)
+library(glue)
+library(tidyhydat)
+library(purrr)
+library(snakecase)
+
+source("dana_utils.R")
+
+ui <- fluidPage(
+  theme = "danamartia.css",
+  titlePanel("DanaMartia"),
+  leafletOutput("stn_map", height = "800px"),
+  hr(),
+  sidebarLayout(
+   
+    sidebarPanel(
+      htmlOutput("stn_text", class = "stn-text"),
+      htmlOutput("stn_start", class = "stn-text"),
+      htmlOutput("thresh_title", class = "stn-text"),
+      tableOutput("thresh_table"),
+      htmlOutput("link_text", class = "stn-text")
+    ),
+    mainPanel(
+      plotOutput("stn_plot", height = "800px")
+    )
+  )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output, session) {
+  
+  load("danamartia_cache.rda")
+  
+  print(names(hy_thresh))
+  
+  # URL for LIO topographic map tiles
+  lio_topo_url <- "https://ws.giscache.lrc.gov.on.ca/arcgis/rest/services/LIO_Cartographic/LIO_Topographic/MapServer"
+
+  withProgress(
+    message = "Gathering data", value = 0, {
+      # Gauge metadata (including coords)
+      gauge_meta <- rtdd::dd_hydro_meta(prov_terr = "ON")
+
+      gauge_meta <- gauge_meta %>%
+        st_as_sf(
+          coords = c(
+            "STATION_LON", "STATION_LAT"
+          )
+        )
+
+      incProgress(1 / 4, detail = "Grabbing real-time station data")
+
+      # All station data
+      gauge_data <- rtdd::dd_hydro_data(
+        prov_terr = "ON", all_stns = TRUE
+      )
+      
+      if(!tidyhydat::hy_version()$Date <= hy_vers_date){
+        
+        incProgress(1 / 4, detail = "Updating historical information, this could take several minutes.")
+        
+        update_thresh(unique(gauge_data$STATION_ID))
+        
+        load("danamartia_cache.rda")
+        
+      }else{
+        thresh_dat <- hy_thresh
+      }
+
+      # # Test with single station
+      # gauge_data <- dd_hydro_data(station_id = "02HA006", prov_terr = "ON")
+
+      # Combine parameter columns
+      gauge_data <- gauge_data %>%
+        gather(
+          "Parameter", "Value",
+          -c(
+            "STATION_ID",
+            "TIMESTAMP",
+            "QA_QC_WL",
+            "QA_QC_DIS"
+          )
+        ) %>%
+        # Clean up parameter names
+        mutate(
+          Parameter = str_replace(
+            Parameter,
+            "WATER_LEVEL",
+            "Water Level (m)"
+          ) %>%
+            str_replace(
+              "DISCHARGE",
+              "Discharge (cms)"
+            )
+        )
+
+      # Filter stations to map to only include
+      # those with values in the data file
+      gauge_meta <- gauge_meta %>%
+        filter(
+          STATION_ID %in% gauge_data$STATION_ID
+        )
+      
+      incProgress(2 / 4, detail = "Gathering historical stats for stations")
+
+      gauge_data <- gauge_data %>%
+        left_join(
+          thresh_dat,
+          by = c(
+            "STATION_ID" = "station_number",
+            "Parameter" = "Parameter"
+          )
+        )
+      
+      incProgress(3 / 4, detail = "Creating map")
+
+      output$stn_map <- renderLeaflet({
+        leaflet(gauge_meta) %>%
+          addEsriTiledMapLayer(lio_topo_url) %>%
+          addCircleMarkers(
+            layerId = ~STATION_ID,
+            clusterOptions = markerClusterOptions()
+          )
+      })
+
+      incProgress(4 / 4, "Donezo")
+    }
+  )
+
+  observeEvent(input$stn_map_marker_click, {
+    stn_data <- gauge_data %>%
+      filter(
+        STATION_ID == input$stn_map_marker_click$id
+      )
+    
+    output$thresh_table <- renderTable({
+      thresh_dat %>%
+        filter(
+          station_number  == input$stn_map_marker_click$id
+        )
+    }, width = "100%")
+
+    
+    # stn_data %>%
+    #   group_by(Parameter) %>%
+    #   summarise(
+    #     "Minimum for period" = min(Value, na.rm = TRUE),
+    #     "Maximum for period" = max(Value, na.rm = TRUE)
+    #   )
+    
+    output$thresh_title <- renderUI({
+      HTML("<b> Percentiles for Period of Record </b><br>")
+    })
+    
+    output$stn_text <- renderUI({
+      HTML(glue(
+        "<b>Station Name: </b><text>{snakecase::to_upper_camel_case(gauge_meta$STATION_NAME[gauge_meta$STATION_ID == input$stn_map_marker_click$id], sep_out = ' ')} <br></text>
+        <b>Station Number: </b><text>{input$stn_map_marker_click$id}</text>"
+      ))
+    })
+
+    # <b>Location: </b><text>{input$stn_map_marker_click$lat}, {input$stn_map_marker_click$lng}<br></text>"
+
+    
+    output$link_text <- renderUI({
+      HTML(glue(
+        "<text><a href='https://wateroffice.ec.gc.ca/report/real_time_e.html?stn={input$stn_map_marker_click$id}' 
+        style = 'color: blue' target='_blank'>View on WSC Website</a>"
+      ))
+    })
+    
+    output$stn_start <- renderUI({
+      HTML(glue(
+        "<b> Active Since: </b><text>{get_min_stn_date(input$stn_map_marker_click$id)}<br></text>"
+      ))
+    })
+
+    output$stn_plot <- renderPlot({
+      plot_stn_data(stn_data)
+    })
+  })
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
---
+++ dana_utils.R
@@ -0,0 +1,112 @@
+# Plotting fun
+plot_stn_data <- function(stn_data) {
+  p_out <- ggplot(
+    stn_data,
+    aes(x = TIMESTAMP, y = Value)
+  ) +
+    geom_point(col = "#047BC1") +
+    geom_line(col = "#439CD1") +
+    # geom_hline(aes(yintercept = `25%`, col = "25%")) +
+    # geom_hline(aes(yintercept = `50%`, col = "50%")) +
+    # geom_hline(aes(yintercept = `75%`, col = "75%")) +
+    # geom_hline(aes(yintercept = `90%`, col = "90%")) +
+    # geom_hline(aes(yintercept = `100%`, col = "100%")) +
+    xlab("Timestamp (UTC)") +
+    ylab(NULL) +
+    theme(
+      panel.grid.minor = element_blank(),
+      panel.background = element_rect(fill = "white"),
+      panel.border = element_rect(size = 1.5, fill = NA),
+      axis.text = element_text(size = 12),
+      axis.line = element_line(size = 1.15),
+      strip.background = element_blank(),
+      strip.text = element_text(size = 14, face = "bold"),
+      strip.placement = "outside"
+    )
+
+  # If discharge values present, add as facet
+  if (sum(is.na(stn_data$Value[stn_data$Parameter == "Discharge (cms"])) < nrow(stn_data)) {
+    p_out <- p_out +
+      facet_wrap(
+        ~Parameter,
+        nrow = 2,
+        scales = "free_y",
+        strip.position = "left"
+      )
+  }
+  return(p_out)
+}
+
+calc_thresh <- function(wsc_id) {
+
+  # Grab data for station (all parameters)
+  station_data <- hy_daily(
+    station_number = wsc_id
+  )
+
+  if (nrow(station_data)) {
+    station_thresh <- station_data %>%
+      # Group by parameter type for stat calculations
+      group_by(Parameter) %>%
+      # Get rid of missing values
+      filter(!is.na(Value)) %>%
+      summarise(
+        "station_number" = wsc_id,
+        # Number of values considered
+        "n" = n(),
+        # Percentiles
+        "25%" = quantile(Value, 0.25),
+        "50%" = quantile(Value, 0.5),
+        "75%" = quantile(Value, 0.75),
+        "90%" = quantile(Value, 0.9),
+        "100%" = quantile(Value, 1)
+      ) %>%
+      mutate_if(is.double, round, 3) %>%
+      ungroup() %>%
+      mutate(
+        Parameter = case_when(
+          Parameter == "Level" ~ "Water Level (m)",
+          Parameter == "Flow" ~ "Discharge (cms)"
+        )
+      )
+  } else {
+    station_thresh <- data.frame(
+      station_number = c(wsc_id, wsc_id),
+      Parmeter = c("Water Level (m)", "Discharge (cms)"),
+      n = c(0, 0),
+      "25%" = c(NA, NA),
+      "50%" = c(NA, NA),
+      "75%" = c(NA, NA),
+      "90%" = c(NA, NA),
+      "100%" = c(NA, NA),
+      stringsAsFactors = FALSE
+    )
+  }
+
+  return(station_thresh)
+}
+
+get_min_stn_date <- function(wsc_id) {
+  tidyhydat::hy_stn_data_range(
+    wsc_id
+  ) %>%
+    pull(
+      Year_from
+    ) %>%
+    min()
+}
+
+update_thresh <- function(wsc_ids, out_path) { 
+  
+  load(out_path)
+  
+  hy_vers_date <- max(tidyhydat::hy_version()$Date)
+  
+  hy_thresh <- purrr::map(
+    unique(wsc_ids),
+    calc_thresh
+  ) %>%
+    bind_rows()
+
+  save(hy_vers_date, hy_thresh[1:8], out_path)
+}
